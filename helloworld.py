be20999abe86572cec996efede204303d6ca5504
manik Shrestha <manikshrestha321@gmail.com>
2024-08-05 17:51:47 +0545

    new

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+Hellllllllllllll World
+echo "# new-repo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Manik251/new-repo.git
+git push -u origin main
